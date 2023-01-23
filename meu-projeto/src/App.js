8f6de8301510e5ac7024977b755abb97f74c4592
Adriel Rodrigues <117696260+AdrielRod@users.noreply.github.com>
2023-01-22 23:45:02 -0300

    estudos de react

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,17 @@
+//Area para importar coisas
+import './App.css';
+//import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa.Js';
+
+//Area de programar.
+function App() {
+
+  return (
+    <div className="App">
+      <SayMyName nome="Adriel" />
+    </div>
+  );
+}
+
+export default App;
---
+++ meu-projeto/src/components/Frase.js
@@ -0,0 +1,11 @@
+function Frase(){
+    return(
+        <div>
+            <p>
+                Este é o meu segundo componente
+            </p>
+        </div>
+    )
+}
+
+export default Frase
